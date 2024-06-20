b909dbbeeab1fd84266bfc448d792553e743e029
du313 <du313@purdue.edu>
2024-06-19 21:54:53 -0400

    update ALNS

--- TEST/solver.py
+++ TEST/solver.py
@@ -58,74 +58,7 @@ def greedy_insertion_init(instance, num_vehicles, vehicle_capacity, battery_capa
 
 
 # ALNS
-class ALNS:
-    def __init__(self, initial_solution, 
-                 removal_operators, repair_operators, 
-                 max_iterations, max_no_improve, segment_length, 
-                 r, weight_update_interval, sigma,
-                 start_temp, cooling_rate,):
-        # solution
-        self.current_solution = initial_solution  
-        self.best_solution = deepcopy(initial_solution) 
-        # Removal and Repair operators
-        self.removal_operators = removal_operators 
-        self.repair_operators = repair_operators
-        # Parameters for ALNS
-        self.max_iterations = max_iterations 
-        self.max_no_improve = max_no_improve 
-        self.weight_update_interval = weight_update_interval  
-        self.segment_length = segment_length  
-        self.r = r
-        # Scores
-        self.sigma1 = sigma[0]
-        self.sigma2 = sigma[1] 
-        self.sigma3 = sigma[2] 
-        # Acceptance criteria
-        self.start_temp = start_temp 
-        self.cooling_rate = cooling_rate  
-        self.current_temp = start_temp  
-        # Visited solutions
-        self.visited_solutions = set() 
 
-        # initialization
-        self.removal_weights = np.ones(len(removal_operators))
-        self.repair_weights = np.ones(len(repair_operators))
-        self.removal_scores = np.zeros(len(removal_operators))
-        self.repair_scores = np.zeros(len(repair_operators))
-    
-    def select_operators(self,weights):
-        '''
-        Select the heuristic algorithms using roulette wheel selection principle
-        The insertion heuristic is selected independently of the removal heuristic
-
-        :param weights: weight lists for different operators
-        :return: index for the selected operator
-        '''
-        total_weight = np.sum(weights)
-        probabilities = weights / total_weight
-        cumulative_probabilities = np.cumsum(probabilities)
-        random_number = random.random()
-        for i, probability in enumerate(cumulative_probabilities):
-            if random_number < probability:
-                return i
-        return len(weights) - 1 # select the last one
-    
-    def update_weight(self):
-        '''
-        At the end of each segement
-        '''
-        pass
-
-    def acceptance_criterion(self, new_solution):
-        pass
-
-
-    def run(self):
-        '''
-        main function
-        :return: the best solution
-        '''
-        pass
 
 
 
